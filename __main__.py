570ebef5861e6f36da8844b5d4f9ab540a36e080
onenora <altriabot@gmail.com>
2023-03-12 09:12:48 +0800

    change main.py workflow

--- __main__.py
+++ __main__.py
@@ -3,21 +3,27 @@
 #
 # 现学现卖、东拼西凑的玩意
 # This is a Python script for running a Pyrogram bot. Created by [author]
-# 2023.03.12
-
+# 2023.03.13
+# 导入“pyrogram”库中的“idle”函数
+# 导入自定义函数“load_plugin”
+# 导入自定义配置变量“client”，“logger”和“prefix”
 from pyrogram import idle
-from utils import import_plugin
+from utils import load_plugin
 from utils.config import client, logger, prefix
 
-def get_bot_greeting():
-    me = client.get_me()
-    return f"Hi~ {me.first_name} {me.last_name}"
-
-def start_bot():
-    import_plugin()
-    client.start()
-    logger.info(f"{get_bot_greeting()}, please send '{prefix}pm help pm' to any chat to get help.")
-
-if __name__ == "__main__":
+# 定义一个名为“main”的函数
+def main():
+    # 调用自定义函数“load_plugin”，加载插件/命令
+    load_plugin()
+    # 使用“client”创建一个会话
     with client:
-        start_bot()
+        # 获取当前“client”所属账号信息
+        me = client.get_me()
+        # 在日志中输出欢迎词
+        logger.info(f"嗨~ {me.first_name}{me.last_name}，请在任意对话框发送 {prefix}pm help pm 获取帮助~")
+        # 让会话保持运行状态
+        idle()
+        
+# 在运行当前脚本时，执行“main”函数
+if __name__ == '__main__':
+    main()
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,19 +1,28 @@
 from pathlib import Path
+from importlib import import_module
+
 from utils.config import plugins_dir, logger
 from utils.utils import scheduler, PLUGINS
 
 scheduler.start()
-PLUGINS.init()
-__import__('utils.plugins_manage')
 
 def import_plugin(plugin):
+    """ 根据插件名称导入对应的python模块 """
     try:
-        __import__(plugin)
+        import_module(plugin)
+        logger.info(f'{plugin} imported successfully')
         return True
     except Exception as e:
-        logger.error(f'failed to import :{plugin}n {e}')
+        logger.error(f'failed to import :{plugin}\n {e}')
         return False
 
-root = Path('data/plugins')
-[import_plugin('.'.join(path.parent.parts + (path.stem,)))
- for path in root.glob('*.py')]
+def load_plugin():
+    # 初始化插件
+    PLUGINS.init()
+    # 导入插件管理模块
+    import_plugin('utils.plugins_manage')
+    # 遍历插件目录下的所有python文件，以数据排序
+    root = Path('data/plugins')
+    for path in sorted(root.glob("*.py")):
+        module_path = '.'.join(path.parent.parts + (path.stem,))
+        import_plugin(module_path)
